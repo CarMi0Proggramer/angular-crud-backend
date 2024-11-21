c82d1fa3ea8ed9d787751cbe648cc895886dbfc8
Carlos <carlosmiguelgarcesbasulto@gmail.com>
2024-11-21 09:56:33 -0500

    cors updated

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppDataSource } from "./data-source";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: "http://localhost:4200",
+        origin: "https://angular-crud.onrender.com",
     });
     await app.listen(process.env.PORT ?? 3000);
 
